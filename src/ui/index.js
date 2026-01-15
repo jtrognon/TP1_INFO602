c12aaa9f6d3766860808ea2bee5edfa41ad2e06c
lena <lena.bechet73@gmail.com>
2026-01-15 22:32:09 +0100

    machine de turing

--- src/ui/index.js
+++ src/ui/index.js
@@ -6,18 +6,75 @@ function main() {
     readFile(result => {
         let mt = new MachineTuring(result);
 
-        name(mt)
+        loadMtUI(mt)
     });
 }
 
 
 
-function name(params) {
-    console.log("toto");
-    
+function loadMtUI(mt) {
+    const currentState = mt.getCurrentState();
+    const tape = mt.getTape();
+    const posCursor = mt.getPosCursor();
+
+    const table = split(tape);
+    execution.appendChild(table);
+
+    const state = document.createElement('div');
+    state.id = "state";
+
+    const cState = document.createElement('div');
+
+    cState.innerText = "État courant : ";
+    cState.append(currentState);
+
+    const nextButton = document.createElement('button');
+    nextButton.type = "button";
+    nextButton.name = "Next";
+    nextButton.innerText = "Suivant";
+
+    console.log(nextButton);
+
+    state.appendChild(cState);
+    state.appendChild(nextButton);
+
+    execution.appendChild(state);
+
+    const idCursor = getIdCursor(posCursor);
+    const element = document.getElementById(idCursor);
+    colorCursor(element);
 }
 
+function split(tape) {
+    const table = document.createElement('div');
+    table.id = "table";
+    for (let i=0; i<tape.length; i++) {
+        const tab = document.createElement('p');
+        tab.id = "tab"+i;
 
+        tab.innerText = tape[i];
+        table.appendChild(tab);
+    }
+    return table;
+}
+
+function getIdCursor(posCursor) {
+    return "tab"+posCursor;
+}
+
+function removeClass() {
+    const previousCursor = document.querySelector(".color");
+
+    if (previousCursor) {
+        previousCursor.classList.remove("color");
+    }
+}
+
+function colorCursor(element) {
+    removeClass();
+
+    element.classList.add("color");
+}
 
 
 const title = document.createElement('h1');
@@ -33,7 +90,6 @@ inputFile.accept = ".mt";
 
 const execution = document.createElement('div');
 execution.id = ("execution");
-
 
 div.appendChild(inputFile);
 div.appendChild(execution);
