0794e2c8830f2705dc53fe446269e0dfcf698987
lena <lena.bechet73@gmail.com>
2026-01-19 18:59:11 +0100

    tab transition

--- src/ui/index.js
+++ src/ui/index.js
@@ -18,6 +18,7 @@ function loadMtUI(mt) {
         const currentState = mt.getCurrentState();
         const tapes = mt.getTapes();
         const posCursors = mt.getPosCursor();
+        const transitions = mt.getTransitions();
 
         for (let i=0; i<tapes.length; i++) {
             const tape = tapes[i];
@@ -36,39 +37,92 @@ function loadMtUI(mt) {
             colorCursor(element);
         }
 
-            const state = document.createElement('div');
-            state.id = "state";
+        const state = document.createElement('div');
+        state.id = "state";
 
-            const cState = document.createElement('div');
-            cState.id = "cState";
+        const cState = document.createElement('div');
+        cState.id = "cState";
 
-            cState.innerText = "État courant : ";
-            cState.append(currentState);
+        cState.innerText = "État courant : ";
+        cState.append(currentState);
 
-            const nextButton = document.createElement('button');
-            nextButton.type = "button";
-            nextButton.name = "Next";
-            nextButton.innerText = "Suivant";
-            nextButton.addEventListener("click", (event) => {
-                mt.next();
-                loadMtUI(mt);
-            })
+        const nextButton = document.createElement('button');
+        nextButton.type = "button";
+        nextButton.name = "Next";
+        nextButton.innerText = "Suivant";
+        nextButton.addEventListener("click", (event) => {
+            mt.next();
+            loadMtUI(mt);
+        })
 
-            state.appendChild(cState);
-            state.appendChild(nextButton);
-            execution.appendChild(state);
+        const tableTransitions = document.createElement('table');
+        tableTransitions.id = "tabTransitions";
 
-            if(mt.isDone()) {
-                const recognized = document.createElement('div');
-                recognized.id = "recognized";
+        const tbody = document.createElement('tbody');
 
-                if(mt.isRecognized()) {
-                    recognized.innerText = "Mot reconnu !";
-                } else {
-                    recognized.innerText = "Mot pas reconnu ...";
-                }
-                execution.appendChild(recognized);
+        // Permet de ne pas avoir de doublons
+        const listeAlphabet = new Set();
+        for (const etat in transitions) {
+            for (const symbole in transitions[etat]) {
+                listeAlphabet.add(symbole);
+            }
         }
+
+        // On transforme listeAlphabet en tableau
+        const alphabet = Array.from(listeAlphabet);
+
+        const alphabetRow = document.createElement('tr');
+        alphabetRow.innerHTML = `<td></td>`;
+        alphabet.forEach(lettre => {
+            const th = document.createElement('th');
+            th.innerText = lettre;
+            alphabetRow.appendChild(th);
+        });
+
+        tbody.appendChild(alphabetRow);
+
+        for (const initialState in transitions){
+            const row = document.createElement('tr');
+
+            const cellEtat = document.createElement('td');
+            cellEtat.innerText = initialState;
+            row.appendChild(cellEtat);
+
+            alphabet.forEach(lettre => {
+                const td = document.createElement('td');
+
+                const cases = transitions[initialState][lettre];;
+                if (cases) {
+                    td.innerText = `(${cases.nextState}, ${cases.nextSymbol}, ${cases.direction})`;
+                } else {
+                    td.innerText = "/";
+                }
+                row.appendChild(td);
+            });
+
+            tbody.appendChild(row);
+            
+        }
+
+        tableTransitions.appendChild(tbody);
+
+        state.appendChild(cState);
+        state.appendChild(nextButton);
+        execution.appendChild(state);
+        execution.appendChild(tableTransitions);
+
+        if(mt.isDone()) {
+            const recognized = document.createElement('div');
+            recognized.id = "recognized";
+
+            if(mt.isRecognized()) {
+                recognized.innerText = "Mot reconnu !";
+            } else {
+                recognized.innerText = "Mot pas reconnu ...";
+            }
+            execution.appendChild(recognized);
+        }
+
     } else {
         const divError = document.createElement('div');
         divError.id = "error";
@@ -105,7 +159,6 @@ function colorCursor(element) {
     element.classList.add("color");
 }
 
-
 const title = document.createElement('h1');
 title.innerText = 'Machine de Turing';
 
@@ -118,7 +171,7 @@ inputFile.name = "file";
 inputFile.accept = ".mt";
 
 const execution = document.createElement('div');
-execution.id = ("execution");
+execution.id = "execution";
 
 div.appendChild(inputFile);
 div.appendChild(execution);
